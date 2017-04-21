7b6e3a1048dedc4a8341a376124975cd57c7e0d5
brijesh <thumarbrijesh@gmail.com>
2017-04-21 18:51:37 +0530

    Initial commit

---
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,15 @@
+package com.app.androidkt.mqtt;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://iot.eclipse.org:1883";
+
+    public static final String PUBLISH_TOPIC = "androidkt/topic";
+
+    public static final String CLIENT_ID = "androidkt";
+}
+
